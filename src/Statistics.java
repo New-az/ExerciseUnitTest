1c9cec95ec221aca46868e44d6939acccb5bd558
ThanaphonKeawjam <tongasura001@gmail.com>
2018-09-02 12:36:06 +0700

    Fix average, variance, and covariance method and add more JUnit

--- src/Statistics.java
+++ src/Statistics.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 /**
  * Methods for computing some common statistics,
  * such as average, variance, and correlation.
+ * 
+ * @author Thanaphon Keawjam
  */
 public class Statistics {
 
@@ -10,9 +12,12 @@ public class Statistics {
 	 * Compute the arithmetic average of an array of numbers.
 	 * @param x is array to compute average of.  Must not be null.
 	 * @return the arithmetic average of values in x, or 0 if x is empty.
+	 * @throws NullpointerException if x is null.
 	 */
 	public static double average(double[] x) {
 		// Some fun: sum using a Stream instead of a loop.
+		if (x == null) throw new NullPointerException();
+		if (x.length == 0) return 0;
 		double sum = Arrays.stream(x).sum();
 		return sum/x.length;
 	}
@@ -26,15 +31,22 @@ public class Statistics {
 	 * @throws IllegalArgumentException if x is empty
 	 */
 	public static double variance(double[] x) {
-		//TODO write the code
-		return 0;
+		double sum = 0;
+		int n = x.length;
+		
+		if (n == 0) throw new IllegalArgumentException();
+		
+		for(int i=0; i<n; i++)
+			sum = sum + (x[i]*x[i]);
+		
+		return sum/n - Math.pow(average(x), 2);
 	}
 	
 	/**
 	 * Compute the covariance between arrays x and y.
 	 * The covariance is defined by
 	 * 
-	 * sum[ (x[k] - average(x)) * (y[k] - average(y)) ]/n
+	 * sum[ (x[k] - average(x)) * (y[k] - average(y)) ]/(n - 1)
 	 * where n is number of elements in the arrays.
 	 * 
 	 * But it is more efficient to compute using the equivalent formula:
@@ -49,8 +61,20 @@ public class Statistics {
 	 * @throws IllegalArgumentException if arrays are not same length or length is 0.
 	 */
 	public static double covariance(double[] x, double[] y) {
-		//TODO write the code
-		return 0;
+		int lenX = x.length;
+		int lenY = y.length;
+		double sum = 0;
+		
+		if(lenX != lenY || lenX == 0 || lenY == 0) throw new IllegalArgumentException();
+		
+		double avgX = average(x);
+		double avgY = average(y);
+		
+		for(int i=0; i<lenX; i++) {
+			sum = sum + ( (x[i] - avgX) * (y[i] - avgY) );
+		}
+		
+		return sum/(lenX-1);
 	}
 	
 }
